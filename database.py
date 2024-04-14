1f16ea317977445017f66135535015314acd3441
Mikołaj Nadra <m.nadra@proton.me>
2024-04-14 19:56:30 +0200

    change database.py structure, create add expense option

--- database.py
+++ database.py
@@ -1,25 +1,58 @@
 import sqlite3
 
+setup_connection = sqlite3.connect('data.db')
+setup_cursor = setup_connection.cursor()
 
-def execute(user_input):
+setup_cursor.execute("""CREATE TABLE IF NOT EXISTS accounts (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                amount REAL NOT NULL)""")
+
+setup_cursor.execute("""CREATE TABLE IF NOT EXISTS expenses (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                amount REAL NOT NULL,
+                account_id INTEGER NOT NULL,
+                FOREIGN KEY (account_id) REFERENCES accounts(id))""")
+
+setup_connection.commit()
+setup_cursor.close()
+setup_connection.close()
+
+
+def view_accounts() -> None:
     connection = sqlite3.connect('data.db')
     cursor = connection.cursor()
 
-    cursor.execute("""CREATE TABLE IF NOT EXISTS budget (
-                   id INTEGER PRIMARY KEY, 
-                   name TEXT, 
-                   amount REAL)""")
+    cursor.execute("SELECT * FROM accounts;")
+    for row in cursor.fetchall():
+        print(f"Account id: {row[0]}, name: {row[1]}, amount: {row[2]}")
 
-    match user_input:
-        case "add_account":
-            data = (input("Enter name: "), float(input("Enter amount: ")))
-            cursor.execute("INSERT INTO budget (name, amount) VALUES (?, ?);", data)
-        case "view_budget":
-            cursor.execute("SELECT name, amount FROM budget;")
-            for row in cursor.fetchall():
-                print(f"Account name: {row[0]}, amount: {row[1]}") 
-        case _:
-            pass
+    cursor.close()
+    connection.close()
+
+
+def add_account(account_name: str, account_amount: float) -> None:
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    cursor.execute("INSERT INTO accounts (name, amount) VALUES (?, ?);", (account_name, account_amount))
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+
+def add_expense(expense_name: str, expense_amount: float, account_id: int) -> None:
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+    
+    try:
+        cursor.execute("INSERT INTO expenses (name, amount, account_id) VALUES (?, ?, ?);", (expense_name, expense_amount, account_id))
+        cursor.execute("UPDATE accounts SET amount = amount - ? WHERE id = ?;", (expense_amount, account_id))
+    except connection.Error:
+        print("Error occurred. Rolling back changes.")
+        connection.rollback()
 
     connection.commit()
     cursor.close()
--- main.py
+++ main.py
@@ -4,14 +4,23 @@ import database as db
 print("""Budget Manager app.
 Enter 1 to see your budget.
 Enter 2 to add account.
+Enter 3 to add expense.
 Enter 0 to exit app.""")
 
 while True:
     match pyip.inputNum("Enter number: "):
         case 1:
-            db.execute("view_budget")
+            db.view_accounts()
         case 2:
-            db.execute("add_account")
+            account_name = pyip.inputStr("Enter account name: ")
+            account_amount = pyip.inputFloat("Enter account amount: ", min=0)
+            db.add_account(account_name, account_amount)
+        case 3:
+            expense_name = pyip.inputStr("Enter expense name: ")
+            expense_amount = pyip.inputFloat("Enter expense amount: ", min=0)
+            db.view_accounts()
+            account_id = pyip.inputInt("Enter account id: ")
+            db.add_expense(expense_name, expense_amount, account_id)
         case 0:
             break
         case _:
